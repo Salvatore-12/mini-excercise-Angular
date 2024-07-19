0ab69205dd83126e31acb32a0857524390a4a55e
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-07-19 10:12:27 +0200

    aggiunto il metodo onSubmit al componente signin

--- src/app/componenti/signin/signin.component.ts
+++ src/app/componenti/signin/signin.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [],
+  imports: [CommonModule ,
+            RouterModule,
+            FormsModule,
+            ReactiveFormsModule,
+            MatFormFieldModule,
+            MatSelectModule,
+            MatInputModule,
+            MatButtonModule,
+            HttpClientModule],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
-export class SigninComponent {
+export class SigninComponent implements OnInit {
+
+   constructor(private authService: AuthService){}
+
+  ngOnInit(): void {}
+
+  onSubmit(form: NgForm){
+    const email = form.value.email
+    const password = form.value.password
+    this.authService.signUp({email: email, password: password,returnSecureToken: true}).subscribe(data =>{
+      console.log(data)
+    })
+    form.reset()
+  }
 
 }
