73e5e19d15d6f3657b6fd22e0c5e35edd91be962
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-07-19 10:55:13 +0200

    aggiunto il metodo signIn  su auth.service.ts

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,7 +5,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-   url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDzAn5R8F-bn_uZ_Bl9rqMMHJwS9jLe894"
+  Apikey ='AIzaSyDzAn5R8F-bn_uZ_Bl9rqMMHJwS9jLe894'
+   url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${this.Apikey}`
+   signInURL = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.Apikey}`
    isLoggin = true
    isAdmin = true
   constructor(private http: HttpClient) { }
@@ -21,5 +23,8 @@ export class AuthService {
   signUp(body: {}){
     return this.http.post(this.url, body)
   }
-
+  
+  signIn(email: string, password: string){
+    return this.http.post(this.signInURL,{email: email, password: password, returnSecureToken: true})
+  }
 }
