8f9d378bae49284f22857386d1a2e05119110032
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-07-21 11:27:31 +0200

    autorizzato il metodo getPersona tramite token su firebase.service.ts

--- src/app/servizi/firebase.service.ts
+++ src/app/servizi/firebase.service.ts
@@ -1,6 +1,7 @@
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +9,15 @@ import { Injectable } from '@angular/core';
 export class FirebaseService {
 
   
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
   insertPersona(url: string, body:{}){
   return this.http.post(url, body) // dove nel primo parametro ci mettiamo url
-                           // mentre nel secondo parametro sara il boby.
+                                   // mentre nel secondo parametro sara il boby.
   }
 
   getPersona(url: string){
-    return this.http.get(url)
+    return this.http.get(`${url}?auth=${this.authService.user?.token}`)
   }
   deletePersona(url: string, id: string){
     return this.http.delete(`${url}/${id}.json`)
